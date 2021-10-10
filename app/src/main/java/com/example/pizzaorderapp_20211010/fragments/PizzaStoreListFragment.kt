582cc038e4c6f80f35d63a4ac01c8139025c489b
이진호 <spris1513@gmail.com>
2021-10-10 10:33:09 +0900

    피자가게 목록 프래그먼트 추가(임시화면)

---
+++ app/src/main/java/com/example/pizzaorderapp_20211010/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,26 @@
+package com.example.pizzaorderapp_20211010.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizzaorderapp_20211010.R
+
+class PizzaStoreListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+}
