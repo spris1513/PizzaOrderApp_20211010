f41216eb0e57f57d3e538e74b80bf48e044442ce
이진호 <spris1513@gmail.com>
2021-10-10 14:55:32 +0900

    Splash엑티비티 메인화면설정

--- app/src/main/java/com/example/pizzaorderapp_20211010/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.pizzaorderapp_20211010.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -32,6 +33,21 @@ class MyProfileFragment : Fragment() {
 
         }
 
+        }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQ_FOR_NICKNAME){
+
+            if(resultCode == Activity.RESULT_OK){
+
+                val newNick = data!!.getStringExtra("nickname")
+                nicknameTxt.text = newNick
+
+            }
+
+        }
 
     }
 
