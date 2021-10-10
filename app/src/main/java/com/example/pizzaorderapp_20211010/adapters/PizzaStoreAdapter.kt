fc50e50d8657bdeef652801db1e3d1c2d418d3a3
이진호 <spris1513@gmail.com>
2021-10-10 11:58:50 +0900

    평문통신 http:// 이미지까지 볼 수 있도록

--- app/src/main/java/com/example/pizzaorderapp_20211010/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/adapters/PizzaStoreAdapter.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.pizzaorderapp_20211010.R
 import com.example.pizzaorderapp_20211010.datas.StoreData
 import java.security.AccessControlContext
@@ -13,7 +16,8 @@ import java.security.AccessControlContext
 class PizzaStoreAdapter(
     val mContext: Context,
     val resId: Int,
-    val mList : ArrayAdapter<StoreData>
+    val mList : ArrayList<StoreData>
+
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
 
     val  mInflater = LayoutInflater.from(mContext)
@@ -26,6 +30,16 @@ class PizzaStoreAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val logoImg = row.findViewById<ImageView>( R.id.logoImg )
+        val storeNameTxt = row.findViewById<TextView>( R.id.storeNameTxt )
+
+        storeNameTxt.text = data.name
+
+        Glide.with(mContext).load(data.logoURL).into(logoImg)
+
 
         return row
 
