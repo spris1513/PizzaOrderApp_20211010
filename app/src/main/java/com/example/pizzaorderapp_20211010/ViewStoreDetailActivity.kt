f715e09677fd4168d79cb3d75ce52a9af62991ca
이진호 <spris1513@gmail.com>
2021-10-10 14:02:57 +0900

    전화걸기 누르면 > 전화(CALL) 권한 확인  - TedPermission으로 권한 체크  - 우선 거절 상황만 코딩

--- app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
@@ -1,9 +1,13 @@
 package com.example.pizzaorderapp_20211010
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.pizzaorderapp_20211010.datas.StoreData
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
@@ -17,6 +21,27 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
         storeNameTxt.text = storeData.name
         phoneNumTxt.text = storeData.phoneNum
+
+        callBtn.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@ViewStoreDetailActivity, "권한이 거절되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
     }
 }
