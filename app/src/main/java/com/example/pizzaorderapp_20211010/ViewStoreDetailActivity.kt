3bef59bc6dcdfd0cefcf0d23aec1c1ff08a6ea03
이진호 <spris1513@gmail.com>
2021-10-10 14:06:36 +0900

    권한 OK일때 > CALL 기능으로 전화 걸기

--- app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pizzaorderapp_20211010
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -26,6 +28,10 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
             val pl = object : PermissionListener {
                 override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${storeData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
 
                 }
 
