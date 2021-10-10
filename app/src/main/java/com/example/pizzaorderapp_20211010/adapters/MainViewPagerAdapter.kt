e9e739d2fbca457fbe169d0fac774ce3542979c6
이진호 <spris1513@gmail.com>
2021-10-10 11:07:23 +0900

    페이지별 제목 return when 활용해서 설정

--- app/src/main/java/com/example/pizzaorderapp_20211010/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/adapters/MainViewPagerAdapter.kt
@@ -7,6 +7,15 @@ import com.example.pizzaorderapp_20211010.fragments.MyProfileFragment
 import com.example.pizzaorderapp_20211010.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+
+        }
+    }
+
     override fun getCount(): Int {
         return 2
     }
