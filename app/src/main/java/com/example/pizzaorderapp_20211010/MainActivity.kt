83c1503b2b12cb86f07739f1ca1836b4d4e23139
이진호 <spris1513@gmail.com>
2021-10-10 11:05:03 +0900

    뷰페이저 / 탭레이아웃 화면 출현

--- app/src/main/java/com/example/pizzaorderapp_20211010/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.pizzaorderapp_20211010
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizzaorderapp_20211010.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewPagerAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mainViewPagerAdapter
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
