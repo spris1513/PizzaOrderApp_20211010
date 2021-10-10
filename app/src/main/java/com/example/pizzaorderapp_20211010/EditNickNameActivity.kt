9c9718c649fa78e8775cd74a9be0d84fba77fdd3
이진호 <spris1513@gmail.com>
2021-10-10 14:34:17 +0900

    확인버튼 누르면 > 닉네임 + ok 신호를 받아서 화면만 복귀

--- app/src/main/java/com/example/pizzaorderapp_20211010/EditNickNameActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/EditNickNameActivity.kt
@@ -1,11 +1,24 @@
 package com.example.pizzaorderapp_20211010
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+
+            val inputNickname = nicknameEdt.text.toString()
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickname",inputNickname)
+            setResult(RESULT_OK,resultIntent)
+            finish()
+
+        }
+
     }
 }
